cd581fd561852de281c9efc60d9ad43a92c00c99
Yuqi <yuqi.wang@veolia.com>
2018-11-11 18:42:39 +0100

    newfiles

--- GenerateDataforTikz.R
+++ GenerateDataforTikz.R
@@ -5,24 +5,27 @@ library(dplyr)
 setwd("D:/Ilustration-proc-dure")
 
 # Generate Data mu1 & r1-----------------------------------------------------------
-Nbre <- 300
+
 Nbre1 <- 50
-Nbre2 <- 150
-Nbre3 <- 100
-NT <- 120
+Nbre2 <- 1000
+Nbre3 <- 20
 Nbre4 <- 20
-a <- 0.001
+Nbre <- Nbre1 + Nbre2 
+NT1 <- 120
+NT2 <- 500
+
+a <- 0.0004
 b <- 0.25
 c <- 0.05
-sd <- 0.05
+sd <- 0.03
 
 Data <- data.frame(N = seq(1:Nbre))
 Data$mu1 <- rep(NA,Nbre)
 
 Data$mu1[1:Nbre1] <- 0
 Data$mu1[(Nbre1+1):(Nbre1 + Nbre2)] <- a*(Data$N[(Nbre1+1):(Nbre1 + Nbre2)] - Nbre1)
-Data$mu1[(Nbre1+Nbre2+1):(Nbre1+Nbre2+Nbre3)] <- a*Nbre2 + b
-Data$mu1[(NT+1):(NT+Nbre4)] <- a*(NT - Nbre1) + c
+Data$mu1[(NT1+1):(NT1+Nbre3)] <- a*(NT1 - Nbre1) + c
+Data$mu1[(NT2+1):(NT2+Nbre4)] <- a*(NT2 - Nbre1) + b
 
 Data$r1 <- Data$mu1 + rnorm(n = Nbre,sd = sd) 
  
@@ -33,22 +36,24 @@ write.table(Data[,c("N","mu1")],"mu1.txt",sep = "\t",row.names = FALSE,col.names
 # Generate Data mu2 & r2-----------------------------------------------------------
 
 
-Nbre <- 300
 Nbre1 <- 50
-Nbre2 <- 150
-Nbre3 <- 100
-NT <- 120
+Nbre2 <- 1000
+Nbre3 <- 20
+Nbre4 <- 20
+Nbre <- Nbre1 + Nbre2 
+NT1 <- 120
+NT2 <- 500
 
-a <- 0.001
-b <- 0.5
-
-sd <- 0.05
+a <- 0.0004
+b <- 0.25
+c <- 0.05
+sd <- 0.03
 
 Data$mu2 <- rep(NA,Nbre)
 
 Data$mu2[1:Nbre1] <- 0
 Data$mu2[(Nbre1+1):(Nbre1 + Nbre2)] <- a*(Data$N[(Nbre1+1):(Nbre1 + Nbre2)] - Nbre1)
-Data$mu2[(Nbre1+Nbre2+1):(Nbre1+Nbre2+Nbre3)] <- a*Nbre2 + b
+Data$mu2[(NT2+1):(NT2+Nbre4)] <- a*(NT2 - Nbre1) - b
 
 
 Data$r2 <- Data$mu2 + rnorm(Nbre,sd = sd) 
@@ -61,20 +66,23 @@ write.table(Data[,c("N","mu2")],"mu2.txt",sep = "\t",row.names = FALSE,col.names
 
 # Labellize the data : 3 trajectory ---------------------------------------
 
-Data$traj[1:Nbre2]  <- "traj1"
-Data$traj[(NT+1):(NT+Nbre4)]  <- "traj2"
-Data$traj[(Nbre1+Nbre2+1):(Nbre1+Nbre2+Nbre3)]  <- "traj3"
+Data$traj[1:(Nbre1 + Nbre2)]  <- "traj1"
+Data$traj[(NT1+1):(NT1+Nbre3)]  <- "traj2"
+Data$traj[(NT2+1):(NT2+Nbre4)]  <- "traj3"
 
 # Delete a part of data to simulate NSS periods ---------------------------
 
-NSSP <- 0.5
+NSSP <- 0.1
+DataBis <- Data[-IndexDelete,]
+DataBis <- DataBis[-sample(1:nrow(DataBis),NSSP*nrow(DataBis)),]
 
-DataBis <- Data[-sample(1:Nbre,NSSP*Nbre),]
+IndexDelete <- c((NT2+50): (Nbre2-100))
+
 
 ggplot(DataBis,aes(x = N, y = r1))  + geom_point() 
 ggplot(DataBis,aes(x = N, y = r2))  + geom_point() 
-ggplot(DataBis,aes(x = N, y = mu1))  + geom_point() 
-ggplot(DataBis,aes(x = N, y = mu2))  + geom_point() 
+ggplot(DataBis,aes(x = r1, y = r2))  + geom_point() 
+ 
 
 
 write.table(DataBis[,c("N","r1")],"r1.txt",sep = "\t",row.names = FALSE,col.names = F)
